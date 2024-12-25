d3f21211750b55d910cca5db8cd5536e8a52081a
Unais Shaikh <unais.shaikh.dev@gmail.com>
2024-12-25 18:08:32 +0530

    use SSR instead different frontend

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT|| 8000;
+const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
 const connectDB = require("./db");
 const router = require("./routes");
-
+const path = require("path");
 //DataBase connection
 connectDB();
 
@@ -13,7 +13,10 @@ connectDB();
 app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public"));
+});
 //routes
 app.use("/api/task", router);
 
